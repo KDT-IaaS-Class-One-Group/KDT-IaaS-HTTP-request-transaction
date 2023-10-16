30471d68b90cf7704401df5e2539e77d1ac9e64d
kongukjae <51992661+kongukjae@users.noreply.github.com>
2023-10-16 21:48:42 +0900

    form 요청처리 controller 기능 추가

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const http = require('http');
 const fs = require('fs');
+// 편리한 사용을 위해 url 내장 모듈을 사용했다.
+const url = require('url');
+
 
 const server = http.createServer((req, res) => {
+
+  const parsedUrl = url.parse(req.url, true); // 구문을 분석하고 URL 객체를 반환한다.
+  // url 내장 모듈이 지원하는 특정 메서드로 parse 는 '구문 분석', 즉 파싱을 편리하게 해준다.
+  console.log("parsedUrl -> ", parsedUrl);
 
   // 가독성을 위한 단순 함수 래핑
   function serverErrorLog() {
@@ -10,9 +17,8 @@ const server = http.createServer((req, res) => {
   }
 
   console.log("어떤 요청이 들어오는지 확인", "url -> ", req.url, "method -> ",  req.method);
-  // 라우팅 처리 제작 두개의 요청 데이터를 확인해야 한다.
-  // 1. 요청 URL
-  // 2. 요청 메서드
+  
+
   if (req.url === '/' && req.method === 'GET') {
     fs.readFile('./static/index.html', 'utf8', (err, data) => {
       if (err) {
@@ -37,10 +43,21 @@ const server = http.createServer((req, res) => {
       res.writeHead(200, { 'Content-Type': 'application/javascript' });
       res.end(data);
     });
-  } else {
+  } else if (req.method === "GET" && parsedUrl.pathname === "/login") {
+    // 해당 조건식은 사용자가 입력 요청이 들어오면 실행된다.
+    // 사용자와 '자동화된 서버'의 상호작용 중 대표적인 사례
+    console.log("form 입력으로부터 받은 데이터 확인 -> ", parsedUrl.query);
+    console.log("form 입력으로부터 받은 데이터 확인 -> ", parsedUrl.query.username);
+    console.log("form 입력으로부터 받은 데이터 확인 -> ", parsedUrl.query.password);
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end("login success!!");
+
+  } 
+  else {
     res.writeHead(404);
     res.end('Not Found');
   }
+
 });
 
 const PORT = 3000;
